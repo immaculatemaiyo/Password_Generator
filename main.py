86eb298c5707796583ffc62390eb27f7dda7ff1c
i_maiyo <i_maiyo.apple.com>
2023-10-03 06:50:42 +0100

    Included a search Button and a pop-up for errors

--- main.py
+++ main.py
@@ -64,6 +64,22 @@ def save():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message = f'Email: {email}\n Password: {password}')
+        else:
+            messagebox.showinfo(title="Error", message=f"No details found for {website}.")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -84,16 +100,19 @@ username_label.grid(row=2, column=0)
 password_label = Label(text="Password:", font=(FONT_NAME, 16))
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
-username_entry = Entry(width=35)
+username_entry = Entry(width=39)
 username_entry.grid(row=2, column=1, columnspan=2)
 username_entry.insert(0, "jelimothoughts@gmail.com")
 
-password_entry = Entry(width=21)
+password_entry = Entry(width=22)
 password_entry.grid(row=3, column=1, columnspan=1)
+
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 
 password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(column=2, row=3)
